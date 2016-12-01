34d7fdc3e43e36e65ad4c4e1e37bd3ed06f07f85
Loreto Parisi <loretoparisi@gmail.com>
2016-12-01 01:22:48 +0100

    commandline initial import

---
+++ tests/example.js
@@ -0,0 +1,44 @@
+/**
+* Node Commandline
+* @author Loreto Parisi (loreto at gmail dot com)
+* @2015-2016 Loreto Parisi
+*/
+(function() {
+
+var CommandLine
+    , cmd
+    , commandItemsArray;
+
+CommandLine = require('../lib/index');
+
+cmd = new CommandLine({ 
+    debug : true, 
+    error : true, 
+    delay : 5 });
+
+commandItemsArray=[
+    ['osascript', '-e', 'tell application "Spotify" to player position as string'],
+    ['osascript', '-e', 'tell application "Spotify" to artist of current track as string'],
+    ['osascript', '-e', 'tell application "Spotify" to name of current track as string']
+];
+cmd.executeCommands(commandItemsArray
+, function(results) {
+    console.log(results);
+}
+, function(error) {
+    console.log(error);
+});
+
+// commandItemsArray is a list of commands list, command options, command arguments
+commandItemsArray = [ ['ls','-l','./'], ['ls','-a','./'] ];
+cmd.executeCommands( commandItemsArray
+, function(results) {
+    console.log( results );
+}
+, function(error) {
+    console.log( error );
+});
+
+
+
+}).call(this);
